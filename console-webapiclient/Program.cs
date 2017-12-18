b6d3456f47aa8310e3fe4b647dc30274f5d445d6
Derek <17056283+derekscode@users.noreply.github.com>
2017-12-17 19:32:06 -0800

    done with Processing the JSON Result

--- console-webapiclient/Program.cs
+++ console-webapiclient/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
 
-namespace console_webapiclient
+namespace WebAPIClient
 {
     class Program
     {
+        private static async Task ProcessRepositories()
+        {
+            var serializer = new DataContractJsonSerializer(typeof(List<repo>));
+
+            var client = new HttpClient();
+            // configured to accept the GitHub JSON responses
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = serializer.ReadObject(await streamTask) as List<repo>;
+
+            foreach (var repo in repositories)
+                Console.WriteLine(repo.name);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ProcessRepositories().Wait();
         }
     }
 }
